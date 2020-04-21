b982f54762138a71ee83569f3b1e6f2cb9e0824c
Fogelman <ddfogelman@gmail.com>
2020-04-21 15:15:20 -0300

    UPDATE welcome & game-over

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -6,8 +6,10 @@ public class GameState {
     public static int round = 0; // Número da partida
     public static int score = 0;
     public static int maxScore = 0;
+    public static Vector3 checkpoint = null;
 
     public static void Restart () {
+        checkpoint = new Vector3 (-12.2, -12.2, 0);
         difficulty = 1; // Nível de dificuldades (de 1 a 3)
         speed = 1.0f; // Velocidade da simulação (de 1 a 2)
         lives = 3; // Número inicial de vídas
