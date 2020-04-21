5fc65cdcc81ae6a329da584ba61a2671b3242944
Fogelman <ddfogelman@gmail.com>
2020-04-21 20:01:46 -0300

    CLEAN up the project

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -1,4 +1,6 @@
 // Classe estática para armazenar os estados do jogo entre as fases
+using UnityEngine;
+
 public class GameState {
     public static int difficulty = 1; // Nível de dificuldades (de 1 a 3)
     public static float speed = 1.0f; // Velocidade da simulação (de 1 a 2)
@@ -6,10 +8,10 @@ public class GameState {
     public static int round = 0; // Número da partida
     public static int score = 0;
     public static int maxScore = 0;
-    public static Vector3 checkpoint = null;
+    public static Vector3 checkpoint = new Vector3 (-12.2f, -12.2f, 0f);
 
     public static void Restart () {
-        checkpoint = new Vector3 (-12.2, -12.2, 0);
+        checkpoint = new Vector3 (-12.2f, -12.2f, 0f);
         difficulty = 1; // Nível de dificuldades (de 1 a 3)
         speed = 1.0f; // Velocidade da simulação (de 1 a 2)
         lives = 3; // Número inicial de vídas
